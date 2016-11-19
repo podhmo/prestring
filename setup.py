210b2bbbcd0ed86a07fb95cf0e9c7450fd1e48b8
podhmo <ababjam61+github@gmail.com>
2016-11-19 17:07:34 +0900

    bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ testing_extras = tests_require + [
 ]
 
 setup(name='prestring',
-      version='0.2.5',
+      version='0.3.0',
       description='source code generation library (with overuse with-syntax)',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
