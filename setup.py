dfd93341381f1bb0b16e0a625f2d7dc6f77587b3
podhmo <ababjam61+github@gmail.com>
2016-12-25 22:36:19 +0900

    sorted

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ testing_extras = tests_require + [
 ]
 
 setup(name='prestring',
-      version='0.4.4',
+      version='0.4.5',
       description='source code generation library (with overuse with-syntax)',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
