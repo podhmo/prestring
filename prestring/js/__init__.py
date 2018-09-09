e0ad6cbfdd5ce1e2f85918c5f8d081e4f6c6b3bd
podhmo <ababjam61+github@gmail.com>
2018-09-10 01:32:10 +0900

    feat: call block

--- prestring/js/__init__.py
+++ prestring/js/__init__.py
@@ -1,9 +1,11 @@
 import contextlib
 import logging
 from .. import Module as _Module
-from .. import (
-    NEWLINE,
+from .. import NEWLINE
+from ..utils import (
+    LazyCall,
     LazyFormat,
+    UnRepr,
 )
 logger = logging.getLogger(__name__)
 
@@ -21,9 +23,15 @@ class JSModule(_Module):
     def stmt(self, fmt, *args, semicolon=None, **kwargs):
         if semicolon is None:
             semicolon = self.semicolon
-        if semicolon and not fmt.endswith(semicolon):
-            fmt = LazyFormat("{}{}", fmt, semicolon)
+        fmt = LazyFormat("{}{}", fmt, semicolon)
         return super().stmt(fmt, *args, **kwargs)
+
+    __call__ = stmt
+    call = staticmethod(LazyCall)
+    symbol = var = staticmethod(UnRepr)
+
+    def assign(self, lhs, rhs):
+        self.stmt(LazyFormat("{} = {}", lhs, rhs))
 
     def return_(self, stmt=None):
         if not stmt:
@@ -48,8 +56,62 @@ class Block:
     def __init__(self, m):
         self.m = m
 
-    def call(self, header):
-        return self._block(header, end="});")
+    def chain(self, header):
+        subm = self.m.submodule(newline=True)
+        subm.semicolon = ""  # disabled
+        self.m = subm  # xxx: side-effect
+        return self._chain(header)
+
+    @contextlib.contextmanager
+    def _chain(self, header):
+        m = self.m
+        m.body.append(header)
+        m.body.append(NEWLINE)
+        with m.scope():
+            yield m
+            m.unnewline()
+            m.stmt(";")
+
+    def object(self, header, *, semicolon=";"):
+        """
+        <header> {
+          <arg0>,
+          <arg1>,
+          ...
+          <argN>
+        };
+        """
+        # todo: rename
+        return self._call_block(header, end="}", semicolon=semicolon)
+
+    def call(self, header, *, semicolon=";"):
+        """
+        <header> {
+          <arg0>,
+          <arg1>,
+          ...
+          <argN>
+        });
+        """
+        # todo: rename
+        return self._call_block(header, end="})", semicolon=semicolon)
+
+    @contextlib.contextmanager
+    def _call_block(self, header, *, end="})", semicolon=";"):
+        m = self.m
+        m.body.append(header)
+        m.body.append(" {")
+        m.body.append(NEWLINE)
+        with m.scope():
+            subm = self.m.submodule(newline=False)
+            subm.semicolon = ","
+            yield subm
+            if not subm.body.is_empty:
+                subm.unnewline()
+                stmt = str(subm.body.pop())
+                subm.body.append(stmt.rstrip(","))
+                subm.body.append(NEWLINE)
+        m.stmt(end, semicolon=semicolon)
 
     def if_(self, header):
         return self._block(LazyFormat("if ({})", header), end="}", semicolon="")
