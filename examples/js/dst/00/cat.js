388ba31183ed1bd5cd29b9132125b5b71864239a
podhmo <ababjam61+github@gmail.com>
2018-09-09 23:36:48 +0900

    adjustment examples

--- examples/js/dst/00/cat.js
+++ examples/js/dst/00/cat.js
@@ -4,9 +4,8 @@ const fs = require("fs");
 program.parse(process.argv);
 const filePath = program.args[0];
 
-fs.readFile(filePath, (err, "utf8", file) =>  {
-  console.log(file);
-  if err {
+fs.readFile(filePath, {"encoding": "utf-8"}, (err, file) =>  {
+  if (err) {
     console.error(err);
     process.exit(err.code);
     return;
--- examples/js/src/00/gen.py
+++ examples/js/src/00/gen.py
@@ -9,8 +9,7 @@ m.stmt("""program.parse(process.argv)""")
 m.stmt("""const filePath = program.args[0]""")
 m.sep()
 # hmm
-with m.brace.call("""fs.readFile(filePath, (err, "utf8", file) => """):
-    m.stmt("""console.log(file)""")
+with m.brace.call("""fs.readFile(filePath, {"encoding": "utf-8"}, (err, file) => """):
     with m.if_("err"):
         m.stmt("""console.error(err)""")
         m.stmt("""process.exit(err.code)""")
