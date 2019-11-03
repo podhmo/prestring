edc12d6303c6db2c095ee88cbb5180ae6e0d59cd
podhmo <ababjam61+github@gmail.com>
2019-11-03 20:42:22 +0900

    refactor: use methods

--- examples/python/src/05/genpkg.py
+++ examples/python/src/05/genpkg.py
@@ -6,46 +6,42 @@ from prestring.output import output
 
 
 @dataclasses.dataclass
-class Config:
+class Context:
     name: str = dataclasses.field(
         default="foo-bar", metadata={"description": "package name"}
     )
     version: str = dataclasses.field(
         default="0.0.0", metadata={"description": "version"}
     )
+    here: str = __file__
+
+    def read_text(self, name: str) -> str:
+        with open(pathlib.Path(self.here).parent / "templates/gitignore") as rf:
+            return (
+                rf.read()
+                .replace("<<c.name>>", str(self.name))
+                .replace("<<c.version>>", str(self.version))
+            )
 
 
-def gen(rootpath: str, c: Config) -> None:
-    with output(rootpath) as fs:
-        with fs.open(f"{c.name}/.gitignore", "w") as wf:
-            with open(pathlib.Path(__file__).parent / "templates/gitignore") as rf:
-                content = (
-                    rf.read()
-                    .replace("<<c.name>>", str(c.name))
-                    .replace("<<c.version>>", str(c.version))
-                )
-            wf.write(content)
-
-        with fs.open(f"{c.name}/README.rst", "w") as wf:
+def gen(rootpath: str, c: Context) -> None:
+    with output(str(pathlib.Path(rootpath) / c.name)) as fs:
+        with fs.open(".gitignore", "w") as wf:
+            wf.write(c.read_text("templates/gitignore"))
+        with fs.open("README.rst", "w") as wf:
             wf.write(f"""{c.name}\n========================================""")
-        with fs.open(f"{c.name}/CHANGES.rst", "w") as wf:
+        with fs.open("CHANGES.rst", "w") as wf:
             wf.write("")
-        with fs.open(f"{c.name}/{c.name}/__init__.py", "w") as wf:
+        with fs.open("__init__.py", "w") as wf:
             wf.write("")
-        with fs.open(f"{c.name}/{c.name}/tests/__init__.py", "w") as wf:
+        with fs.open("tests/__init__.py", "w") as wf:
             wf.write("")
-        with fs.open(f"{c.name}/setup.py", "w") as wf:
-            with open(pathlib.Path(__file__).parent / "templates/setup.py") as rf:
-                content = (
-                    rf.read()
-                    .replace("<<c.name>>", str(c.name))
-                    .replace("<<c.version>>", str(c.version))
-                )
-            wf.write(content)
+        with fs.open("setup.py", "w") as wf:
+            wf.write(c.read_text("templates/setup.py"))
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     root_path = sys.argv[1]
-    c = Config()
+    c = Context()
     gen(root_path, c)
