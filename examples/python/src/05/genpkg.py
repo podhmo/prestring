3ab20cd633e6fe4194471cf76d3cce3fc76abd3a
podhmo <ababjam61+github@gmail.com>
2019-11-03 20:36:04 +0900

    fix: use external files

--- examples/python/src/05/genpkg.py
+++ examples/python/src/05/genpkg.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import dataclasses
+import pathlib
 from prestring.output import output
 
 
@@ -15,138 +16,32 @@ class Config:
 
 
 def gen(rootpath: str, c: Config) -> None:
-    file2 = f"{c.name}/.gitignore"
-    val0 = """# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-
-# C extensions
-*.so
-
-# Distribution / packaging
-.Python
-env/
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-*.egg-info/
-.installed.cfg
-*.egg
-
-# PyInstaller
-#  Usually these files are written by a python script from a template
-#  before PyInstaller builds the exe, so as to inject date/other infos into it.
-*.manifest
-*.spec
-
-# Installer logs
-pip-log.txt
-pip-delete-this-directory.txt
-
-# Unit test / coverage reports
-htmlcov/
-.tox/
-.coverage
-.coverage.*
-.cache
-nosetests.xml
-coverage.xml
-
-# Translations
-*.mo
-*.pot
-
-# Django stuff:
-*.log
-
-# Sphinx documentation
-docs/_build/
-
-# PyBuilder
-target/
-""".replace(
-        "<<c.name>>", str(c.name)
-    ).replace(
-        "<<c.version>>", str(c.version)
-    )
-    file3 = f"{c.name}/README.rst"
-    val1 = """<<c.name>>========================================""".replace(
-        "<<c.name>>", str(c.name)
-    ).replace("<<c.version>>", str(c.version))
-
-    file4 = f"{c.name}/CHANGES.rst"
-    val2 = """""".replace("<<c.name>>", str(c.name)).replace(
-        "<<c.version>>", str(c.version)
-    )
-    file6 = f"{c.name}/{c.name}/__init__.py"
-    file8 = f"{c.name}/{c.name}/tests/__init__.py"
-    file9 = f"{c.name}/setup.py"
-    val3 = """import os
-from setuptools import(
-    setup,
-    find_packages
-)
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-try:
-    with open(os.path.join(here, 'README.rst')) as f:
-        README = f.read()
-    with open(os.path.join(here, 'CHANGES.rst')) as f:
-        CHANGES = f.read()
-except IOError:
-    README = CHANGES = ''
-
-
-install_requires = []
-docs_extras = []
-tests_requires = []
-testing_extras = tests_requires + []
-
-
-setup(
-    name='<<c.name>>',
-    version='<<c.version>>',
-    classifiers=['Programming Language :: Python', 'Programming Language :: Python :: Implementation :: CPython'],
-    keywords='',
-    author='',
-    author_email='',
-    url='',
-    packages=find_packages(exclude=['<<c.name>>.tests']),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    extras_require={
-   'testing': testing_extras,
-   'docs': docs_extras},
-    tests_require=tests_requires,
-    test_suite='<<c.name>>.tests',
-    entry_points='')""".replace(
-        "<<c.name>>", str(c.name)
-    ).replace(
-        "<<c.version>>", str(c.version)
-    )
     with output(rootpath) as fs:
-        with fs.open(file2, "w") as wf:
-            wf.write(val0)
-        with fs.open(file3, "w") as wf:
-            wf.write(val1)
-        with fs.open(file4, "w") as wf:
-            wf.write(val2)
-        with fs.open(file6, "w") as wf:
-            wf.write(val2)
-        with fs.open(file8, "w") as wf:
-            wf.write(val2)
-        with fs.open(file9, "w") as wf:
-            wf.write(val3)
+        with fs.open(f"{c.name}/.gitignore", "w") as wf:
+            with open(pathlib.Path(__file__).parent / "templates/gitignore") as rf:
+                content = (
+                    rf.read()
+                    .replace("<<c.name>>", str(c.name))
+                    .replace("<<c.version>>", str(c.version))
+                )
+            wf.write(content)
+
+        with fs.open(f"{c.name}/README.rst", "w") as wf:
+            wf.write(f"""{c.name}\n========================================""")
+        with fs.open(f"{c.name}/CHANGES.rst", "w") as wf:
+            wf.write("")
+        with fs.open(f"{c.name}/{c.name}/__init__.py", "w") as wf:
+            wf.write("")
+        with fs.open(f"{c.name}/{c.name}/tests/__init__.py", "w") as wf:
+            wf.write("")
+        with fs.open(f"{c.name}/setup.py", "w") as wf:
+            with open(pathlib.Path(__file__).parent / "templates/setup.py") as rf:
+                content = (
+                    rf.read()
+                    .replace("<<c.name>>", str(c.name))
+                    .replace("<<c.version>>", str(c.version))
+                )
+            wf.write(content)
 
 
 if __name__ == "__main__":
--- examples/python/src/05/templates/setup.py
+++ examples/python/src/05/templates/setup.py
@@ -20,8 +20,8 @@ testing_extras = tests_requires + []
 
 
 setup(
-    name="foo-bar",
-    version="0.0.0",
+    name="<<c.name>>",
+    version="<<c.version>>",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: Implementation :: CPython",
@@ -30,12 +30,12 @@ setup(
     author="",
     author_email="",
     url="",
-    packages=find_packages(exclude=["foo-bar.tests"]),
+    packages=find_packages(exclude=["<<c.name>>.tests"]),
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
     extras_require={"testing": testing_extras, "docs": docs_extras},
     tests_require=tests_requires,
-    test_suite="foo-bar.tests",
+    test_suite="<<c.name>>.tests",
     entry_points="",
 )
