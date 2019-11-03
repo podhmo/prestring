8b8cf0083d25684696395d1f7d8a7ff62657b831
podhmo <ababjam61+github@gmail.com>
2019-11-03 23:54:57 +0900

    docs: update examples

--- examples/python/dst/07/hello.template.py
+++ examples/python/dst/07/hello.template.py
@@ -1,15 +1,21 @@
-from prestring.python import Module
-m = Module()  # noqa
+from prestring.python import PythonModule
 import textwrap
 
 
-with m.def_('hello', 'name', '*', 'message: str =  "hello world"'):
-    m.docstring(textwrap.dedent("""
-    greeting message
-    """).strip())
-    m.stmt('print(f"{name}: {message}")')
+def gen(*, m=None, indent='    '):
+    m = m or PythonModule(indent=indent)
+
+    with m.def_('hello', 'name', '*', 'message: str =  "hello world"'):
+        m.docstring(textwrap.dedent("""
+        greeting message
+        """).strip())
+        m.stmt('print(f"{name}: {message}")')
+
+    with m.if_('__name__ == "__main__"'):
+        m.stmt('hello("foo")')
+    return m
 
 
-with m.if_('__name__ == "__main__"'):
-    m.stmt('hello("foo")')
-print(m)
+if __name__ == "__main__":
+    m = gen(indent='    ')
+    print(m)
