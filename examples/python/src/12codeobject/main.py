b7b3aaf83217ec86976d2bad2829349fd2ad2e31
podhmo <ababjam61+github@gmail.com>
2020-02-23 02:19:16 +0900

    chore: add examples

---
+++ examples/python/src/12codeobject/main.py
@@ -0,0 +1,23 @@
+from prestring.python import Module
+from prestring.codeobject import CodeobjectModule
+
+m = Module()
+co = CodeobjectModule(m)
+re = co.import_("re")
+sys = co.import_("sys")
+
+m.sep()
+pattern = co.let(
+    "pattern",
+    re.compile(
+        r"^(?P<label>DEBUG|INFO|WARNING|ERROR|CRITICAL):\s*(?P<message>\S+)",
+        re.IGNORECASE,
+    ),
+)
+
+
+with m.for_("line", sys.stdin):
+    matched = co.let("matched", pattern.search(co.symbol("line")))
+    with m.if_(f"{matched} is not None"):
+        m.stmt(co.symbol("print")(matched.groupdict()))
+print(m)
