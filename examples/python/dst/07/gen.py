05bfc19fb31b1e237da334732e52d61d48da0e6b
podhmo <ababjam61+github@gmail.com>
2020-02-22 22:11:08 +0900

    chore: add broken example

--- examples/python/dst/07/gen.py
+++ examples/python/dst/07/gen.py
@@ -4,15 +4,18 @@ from prestring.python import PythonModule
 def gen(*, m=None, indent='    '):
     m = m or PythonModule(indent=indent)
 
-    import textwrap
-    with m.def_('hello', 'name', '*', 'message: str =  "hello world"'):
-        m.docstring(textwrap.dedent("""
-        greeting message
-        """).strip())
-        m.stmt('print(f"{name}: {message}")')
+    with m.def_('fizzbuzz', 'n: int', return_type='str'):
+        with m.if_('n % 3 == 0 and n % 5 == 0'):
+            m.stmt('return "fizzbuzz"')
+        with m.elif_('n % 3 == 0'):
+            m.stmt('return "fizz"')
+        with m.elif_('n % 5 == 0'):
+            m.stmt('return "buzz"')
+        with m.else_(''):
+            m.stmt('return str(n)')
 
     with m.if_('__name__ == "__main__"'):
-        m.stmt('hello("foo")')
+        m.stmt('print(", ".join(fizzbuzz(i) for i in range(1, 21)))')
     return m
 
 
