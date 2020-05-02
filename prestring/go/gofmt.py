869b07171996a0c37ccbbf4c28ca2d855c7183ae
podhmo <ababjam61+github@gmail.com>
2020-05-03 06:07:35 +0900

    fix: fix for 3.6

--- prestring/go/gofmt.py
+++ prestring/go/gofmt.py
@@ -1,11 +1,16 @@
 import typing as t
+import sys
 import os
 import subprocess
 import tempfile
 
 
 def gofmt(
-    code: str, *, always: bool = True, cmd: t.Optional[t.List[str]] = None
+    code: str,
+    *,
+    always: bool = True,
+    cmd: t.Optional[t.List[str]] = None,
+    encoding: str = "utf-8"
 ) -> str:
     if always or not bool(os.environ.get("GOFMT")):
         return code
@@ -14,5 +19,13 @@ def gofmt(
     with tempfile.TemporaryFile("w+") as wf:
         wf.write(code)
         wf.seek(0)
-        p = subprocess.run(cmd, stdin=wf, stdout=subprocess.PIPE, text=True, check=True)
-        return p.stdout
+
+        # fixme
+        if sys.version_info < (3, 7):
+            p = subprocess.run(cmd, stdin=wf, stdout=subprocess.PIPE, check=True)
+            return p.stdout.encode(encoding)
+        else:
+            p = subprocess.run(
+                cmd, stdin=wf, stdout=subprocess.PIPE, text=True, check=True
+            )
+            return p.stdout
